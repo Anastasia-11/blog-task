7540a5212cde841b8800efb3bf9f282baa2fa5b2
Anastasiya Fedorovich <anastasiaf0311@yandex.by>
2022-03-15 00:03:51 +0300

    add ArticleController with views

---
+++ blog-web-app/Controllers/ArticleController.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics;
+using blog_web_app.Services;
+using blog_web_app.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace blog_web_app.Controllers;
+
+public class ArticleController : Controller
+{
+    private readonly IArticleService _articleService;
+
+    public ArticleController(IArticleService articleService)
+    {
+        _articleService = articleService;
+    }
+
+    public IActionResult List() => View(_articleService.Articles);
+
+    public IActionResult Index(Guid id) => View(_articleService.GetArticleById(id));
+    
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
