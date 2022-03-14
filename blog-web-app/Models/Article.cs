6bc4471e4e0d5054ab4a0573f20aa11f3d59390e
Anastasiya Fedorovich <anastasiaf0311@yandex.by>
2022-03-14 23:58:58 +0300

    create models Article, Category, Tag

---
+++ blog-web-app/Models/Article.cs
@@ -0,0 +1,9 @@
+﻿namespace blog_web_app.Models;
+
+public class Article
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; set; }
+    public string? ShortDescription { get; set; }
+    public string Description { get; set; }
+}
---
+++ blog-web-app/Models/Tag.cs
@@ -0,0 +1,7 @@
+﻿namespace blog_web_app.Models;
+
+public class Tag
+{
+    public Guid Id { get; } = Guid.NewGuid();
+    public string Name { get; set; }
+}
