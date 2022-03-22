e89b803e12ff13a7d0b58abe23322c156990bcb9
Anastasiya Fedorovich <anastasiaf0311@yandex.by>
2022-03-22 14:03:38 +0300

    add category filter

--- blog-web-app/Controllers/ArticleController.cs
+++ blog-web-app/Controllers/ArticleController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using blog_web_app.Models;
 using blog_web_app.Services;
 using blog_web_app.ViewModels;
 using Microsoft.AspNetCore.Mvc;
@@ -8,13 +9,29 @@ namespace blog_web_app.Controllers;
 public class ArticleController : Controller
 {
     private readonly IArticleService _articleService;
+    private readonly ICategoryService _categoryService;
 
-    public ArticleController(IArticleService articleService)
+    public ArticleController(IArticleService articleService, ICategoryService categoryService)
     {
         _articleService = articleService;
+        _categoryService = categoryService;
     }
 
-    public IActionResult List() => View(_articleService.Articles);
+    public IActionResult List(Guid? categoryId)
+    {
+        var categories = _categoryService.Categories;
+        categories.Prepend(new Category { Id = Guid.NewGuid(), Name = "Все" });
+ 
+        var model = new ArticleListViewModel
+        {
+            Categories = categories,
+            Articles = categoryId != null 
+                ? _articleService.Articles.Where(a => a.CategoryId == categoryId) 
+                : _articleService.Articles
+        };
+
+        return View(model);  
+    }
 
     public async Task<IActionResult> ArticleInfo(Guid id)
     {
---
+++ blog-web-app/ViewModels/ArticleListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using blog_web_app.Models;
+
+namespace blog_web_app.ViewModels;
+
+public class ArticleListViewModel
+{
+    public IQueryable<Article> Articles { get; set; }
+    public IQueryable<Category> Categories { get; set; }
+}
